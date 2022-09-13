a95024b9946162f35a71d8fd11e14df4fdade725
Juan Mosquella <jmosquella11@gmail.com>
2022-09-13 18:42:12 -0300

    agregado cv

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import './hero.css';
-import JuanMosquellaCV from '../../../mosquellaCV.pdf';
+import JuanMosquellaCV from '../../mosquellaCV.pdf';
 import { useTypewriter} from 'react-simple-typewriter'
 import { useContext } from 'react';
 import { ThemeContext } from '../../context/ThemeProvider';
